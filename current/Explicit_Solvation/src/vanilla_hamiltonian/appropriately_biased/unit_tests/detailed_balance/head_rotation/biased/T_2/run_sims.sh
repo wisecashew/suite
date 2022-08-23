e62890d73ad7143e91e9ac1a8b9d49297b3a2749
Satyen Dhamankar <satyend@princeton.edu>
2022-08-22 21:00:07 -0400

    added unit tests

--- current/Explicit_Solvation/src/vanilla_hamiltonian/appropriately_biased/unit_tests/detailed_balance/head_rotation/biased/T_2/run_sims.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-echo "running from initial to final..."
-./hr_o_n_boltz -f 10000 -M 1'000'000 -p initial.txt -t geom_and_esurf.txt -u energy -L lattice -e orientation -s stats -o coords >> o_to_n 
-
-echo "running from final to initial..."
-./hr_n_o_boltz -f 10000 -M 1'000'000 -p final.txt -t geom_and_esurf.txt -u energy -L lattice -e orientation -s stats -o coords >> n_to_o
---
+++ current/Explicit_Solvation/src/vanilla_hamiltonian/appropriately_biased/unit_tests/detailed_balance/head_rotation/biased/TwoBead/T_2/get_ratios.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+T=`grep 'Temperature' o_to_n | grep -oE '\d+\.\d+|\d+'`
+hits_on=`grep 'hits' o_to_n | grep -oE '\d+'`
+hits_no=`grep 'hits' n_to_o | grep -oE '\d+'`
+Eo=`grep 'Energy of system' o_to_n | grep -oE '\d+\.\d+|\d+'`
+En=`grep 'Energy of system' n_to_o | grep -oE '\d+\.\d+|\d+'`
+# b=$(( -1/$T * (En-Eo) ))
+
+lhs=$(awk "BEGIN{print $hits_on/$hits_no }")
+echo "Ratio of acceptance probabilities (empirical) = $lhs"
+
+rhs=$(awk "BEGIN{print exp(1/$T * ($En-$Eo)) }")
+echo "Ratio of transition probabilities (ground truth) = $rhs"
+
+# echo "$T $hits_on $hits_no $Eo $En"
+

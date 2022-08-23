90611ca9c4361abecbcc1e01b1b9be805d13f941
Satyen Dhamankar <satyend@princeton.edu>
2022-08-23 01:43:37 -0400

    prepped some unit tests

---
+++ current/Explicit_Solvation/src/vanilla_hamiltonian/appropriately_biased/unit_tests/detailed_balance/tail_rotation/biased/TwoBead/T_3/print_ratios.py
@@ -0,0 +1,36 @@
+import numpy as np 
+import re
+
+temp_str = "Temperature"
+hits = "hits" 
+energy = "Energy of system"
+
+for k in ["1", "2", "3"]:
+    f = open("n_to_o_"+k, 'r') 
+    for line in f:
+        if re.search (temp_str, line):
+            res = re.search ("\d+\.\d+|\d+", line)
+            T = float(res.group(0))
+        elif re.search(energy, line):
+            res = re.search("\d+\.\d+|\d+", line)
+            En  = float(res.group(0)) 
+        elif re.search (hits, line):
+            res = re.search ("\d+", line) 
+            hits_no = float(res.group(0)) 
+            break 
+    f.close() 
+    f = open("o_to_n_"+k, 'r')
+    for line in f:
+        if re.search (energy, line):
+            res = re.search("\d+\.\d+|\d+", line)
+            Eo  = float(res.group(0)) 
+        elif re.search (hits, line):
+            res = re.search ("\d+", line)
+            hits_on = float(res.group(0))
+            break 
+
+    f.close() 
+
+    print (k+". Ratio of empirical transitions (predicted) = ", hits_on/hits_no)
+    print (k+". Ratio of unbiased transitions (truth) = ", np.exp(1/T*(En-Eo)))
+

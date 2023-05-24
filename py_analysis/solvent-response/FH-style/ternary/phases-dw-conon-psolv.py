0a30b5891996e7df249a5c5931261a79e69c8e39
Satyen Dhamankar <satyend@princeton.edu>
2023-05-24 17:26:26 -0400

    added a solvent response python notebook

--- py_analysis/solvent-response/FH-style/ternary/phases-dw-conon-psolv.py
+++ py_analysis/solvent-response/FH-style/ternary/phases-dw-conon-psolv.py
@@ -1,4 +1,4 @@
-#!/Users/satyendhamankar/opt/anaconda3/envs/GBCG/bin/python
+#!/Users/satyend/opt/anaconda3/envs/CG/bin/python
 
 import numpy as np
 import matplotlib 
@@ -13,6 +13,7 @@ from matplotlib.ticker import StrMethodFormatter
 from matplotlib.ticker import Locator
 import mpltern
 import sys
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -42,25 +43,24 @@ if __name__=="__main__":
 
     meshsize            = 1000
     phi_a               = np.linspace (0.01, 1-0.01, meshsize)
-    chi_ab              = -1
-    chi_bc              = -1
-    chi_ac              = -12
+    chi_ab              = 2
+    chi_bc              = 2
+    chi_ac              = 2
 
     tern = roots (phi_a, chi_ab, chi_bc, chi_ac)
     r1   = tern[0]
     r2   = tern[1]
 
-    to_keep = (~np.isnan(r1)) * (r1 <= 1) * (r1 >= 0) * (~np.isnan(r2)) * (r2 <= 1) * (r2 >= 0)
+    to_keep_1 = (~np.isnan(r1)) * (r1 <= 1) * (r1 >= 0) # * (~np.isnan(r2)) * (r2 <= 1) * (r2 >= 0)
 
-    r1    = r1[to_keep]
+    ax.scatter(phi_a[to_keep_1], r1[to_keep_1], 1-phi_a[to_keep_1]-r1[to_keep_1], color='steelblue', s=1)
+
+    to_keep_2 = (~np.isnan(r2)) * (r1 <= 2) * (r1 >= 0)
+
     r2    = r2[to_keep]
     phi_a = phi_a[to_keep]
 
-    # print (r1)
-    
-    # Plot the points
-    ax.scatter(phi_a, r1, 1-phi_a-r1, color='steelblue', s=1)
-    ax.scatter(phi_a, r2, 1-phi_a-r2, color='coral', s=1)
+    ax.scatter(phi_a[to_keep_2], r1[to_keep_2], (1-phi_a[to_keep_2]-r2[to_keep_2]), color='steelblue', s=1)
 
     ax.set_tlabel('Vol. frac. A')
     ax.set_llabel('Vol. frac. B')
@@ -72,20 +72,15 @@ if __name__=="__main__":
     ax.set_rlim(0, 1)
     # ax.ticks(axis='lbr', multiple=5, linewidth=1, offset=0.025)
 
+    ax.laxis.set_major_locator (AutoMinorLocator (6))
+
     positions = ['tick1', 'tick2']
     for position in positions:
         ax.taxis.set_ticks_position(position)
         ax.laxis.set_ticks_position(position)
         ax.raxis.set_ticks_position(position)
 
-    # Add gridlines
-    # gridlines_per_axis = 100
-    # ax.grid(linewidth=0.5, color='gray', linestyle='dotted', multiple=gridlines_per_axis)
+
     ax.grid()
 
     plt.savefig ("ternary", dpi=1200)
-    """
-    print ("Computation completed.")
-
-    # print ("solvent_phi_range = ",solvent_phi_range)
-    """
